8703ec20efcf3cd97b66796896be2ae6a596a839
D-Yusuf <alnasiriyusuf@gmail.com>
2023-02-25 11:09:44 +0300

    done, just needs responsive design

---
+++ unit-converter/src/app.js
@@ -0,0 +1,54 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+let inputEl = document.getElementById("input-el");
+let convertBtn = document.getElementById("convert-btn");
+let firstText = document.getElementById("meter-feet");
+let secondText = document.getElementById("litre-gallon");
+let thirdText = document.getElementById("kilogram-pound");
+const metric = {
+  meter: (value) => {
+    return (value / 3.281).toFixed(3);
+  },
+  litre: (value) => {
+    return (value / 0.264).toFixed(3);
+  },
+  kilogram: (value) => {
+    return (value / 2.204).toFixed(3);
+  },
+};
+const imperial = {
+  foot: (value) => {
+    return (value * 3.281).toFixed(3);
+  },
+  gallon: (value) => {
+    return (value * 0.264).toFixed(3);
+  },
+  pound: (value) => {
+    return (value * 2.204).toFixed(3);
+  },
+};
+
+
+function compare(changeText, value, metricKey, imperialKey, metricFunction, imperialFunction) {
+    
+    changeText.innerText = `${value} ${metricKey} = ${imperialFunction} ${imperialKey} | ${value} ${imperialKey} = ${metricFunction} ${metricKey}`
+  
+}
+
+convertBtn.addEventListener("click", ()=>{
+    let value = inputEl.value
+    compare(firstText,value, "meter", "feet", metric.meter(value), imperial.foot(value))
+    compare(secondText,value, "litres", "gallons", metric.litre(value), imperial.gallon(value))
+    compare(thirdText,value, "kilograms", "pounds", metric.kilogram(value), imperial.pound(value))
+
+
+})
+// console.log(imperial.foot(20));
+// console.log(imperial.gallon(20));
+// console.log(imperial.pound(20));
+// console.log(metric.meter(20));
+// console.log(metric.litre(20));
+// console.log(metric.kilogram(20));
--- unit-converter/tailwind.config.cjs
+++ unit-converter/tailwind.config.cjs
@@ -4,7 +4,16 @@ module.exports = {
     "./index.html",
   "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "primary": "#1C1C1C",
+        "purple": "#6943FF",
+        "main":"#F4F4F4",
+        "title": "#5A537B",
+        "info": "#353535"
+      }
+    },
+    
   },
   plugins: [],
 }
